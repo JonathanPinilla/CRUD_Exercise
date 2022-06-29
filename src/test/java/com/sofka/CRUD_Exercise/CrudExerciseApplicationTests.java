697cc3114512b0abdd2422f75a343705ef12b233
Jonathan Daniel Pinilla Forero <jonathan_pinilla@outlook.com>
2022-06-28 23:11:46 -0500

    First commit, spring boot template

---
+++ src/test/java/com/sofka/CRUD_Exercise/CrudExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.CRUD_Exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
